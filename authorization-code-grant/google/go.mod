a6aa66868d7f9dcd1fae87fd962f9919cdd71cce
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-08-14 23:45:35 -0300

    Add authorization code wih google provider

---
+++ authorization-code-grant/google/go.mod
@@ -0,0 +1,8 @@
+module github.com/maxsuelmarinho/golang-oauth-example/authorization-code-grant/google
+
+go 1.12
+
+require (
+	github.com/google/uuid v1.1.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+)
---
+++ authorization-code-grant/google/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/google/uuid"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var googleOAuthConfig *oauth2.Config
+var oauthStateString = uuid.New().String()[:8]
+
+func init() {
+	googleOAuthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/callback", handleCallback)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	var htmlIndex = `<html><body><a href="/login">Google Log in</a></body><html>`
+	fmt.Fprintf(w, htmlIndex)
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOAuthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	content, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	fmt.Fprintf(w, "Content: %s\n", content)
+}
+
+func getUserInfo(state string, code string) ([]byte, error) {
+	if state != oauthStateString {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+	token, err := googleOAuthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+	fmt.Println("token: " + token.AccessToken)
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	return contents, nil
+}
